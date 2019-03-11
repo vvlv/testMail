c95e6ef6d43d3eca7e60cceeeaee30246adb6fc4
serge <vavilovlsergey@gmail.com>
2019-03-11 22:14:03 +0300

    тперь чтото работает

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -22,10 +22,11 @@ public class FirstTest extends WebDriverSettings {
         //3. Выбираем день "Завтра"
 WebElement today = driver.findElementByXPath("//div[@class='input-group input-group_fixed']//div[@class='dropdown__field js-dates__toggle']");
 today.click();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//WebElement tommorowSelect = driver.findElementByXPath("//div[@class='suggest suggest_active dropdown__suggest']//input[@data-title='Завтра']");
+//tommorowSelect.click();
 
-WebElement todaylist = driver.findElementByXPath( "//div[@class='suggest__inner js-scrollable__view dropdown__scroll']//label[@class='checkbox__side checkbox__label']");
-todaylist.click();
+
+
 
         //4. Вводим метро "Курская" и выбираем из предложенных вариантов Курская (кольцевая)
         WebElement metro = driver.findElementByXPath("//div[@class='input-group input-group_fixed']//input[@class='input__field js-suggest__input']");
@@ -33,54 +34,28 @@ todaylist.click();
         metro.sendKeys("Курская");
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        Select select = new Select(metro);
+ WebElement kyrskay = driver.findElementByXPath("//div[@class='suggest js-suggest__list']//div[@class='suggest__item js-suggest__item']");
+ kyrskay.click();
 
-        select.getOptions();
-        select.selectByIndex(2);
-        //5. Выставляем жанр "Драма" и "Комедия"
-        WebElement drama = driver.findElementByXPath("/html/body/div[2]/div[9]/div[4]/div/div[5]/div/form/div[1]/div[3]/div/div[1]/div[2]");
+    //5. Выставляем жанр "Драма" и "Комедия"
+    /*    WebElement drama = driver.findElementByXPath("/html/body/div[2]/div[9]/div[4]/div/div[5]/div/form/div[1]/div[3]/div/div[1]/div[2]");
 drama.click();
 WebElement dramaone = driver.findElementByXPath("/html/body/div[2]/div[9]/div[4]/div/div[5]/div/form/div[1]/div[3]/div/div[2]/div/div[1]/div[1]/label");
 dramaone.click();
 WebElement comedy = driver.findElementByXPath("/html/body/div[2]/div[9]/div[4]/div/div[5]/div/form/div[1]/div[3]/div/div[2]/div/div[1]/div[2]/label");
 comedy.click();
-
+*/
         //6. Ставим чекбокс "Только сеансы в 2D"
-        Wait<WebDriver> wait = new WebDriverWait(driver, 4, 1000);
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div[9]/div[3]/div/div[5]/div/form/div[2]/span[1]/div/div[1]/label")));
-
-        WebElement onlyTwoD = driver.findElementByXPath(("/html/body/div[2]/div[9]/div[3]/div/div[5]/div/form/div[2]/span[1]/div/div[1]/label"));
-        //      onlyTwoD.click(); /html/body/div[2]/div[9]/div[3]/div/div[5]/div/form/div[2]/span[1]/div/div[1]/label
-        //      WebElement onlyTwoD = driver.findElement(By.xpath("//div[@class='checkbox checkbox_colored margin_right_20']//label[@class='checkbox__side checkbox__label']"));
-        Actions actions = new Actions(driver);
-        actions.moveToElement(onlyTwoD).click().perform();
-        //    onlyTwoD.click();
-
-
-
-
+          WebElement onlyTwoD = driver.findElementByXPath(("//div[@class='js-module']//label[@class='checkbox__side checkbox__label']"));
+          onlyTwoD.click();
         //7. Жмем "Подобрать"
-        WebElement podbor = driver.findElementByName("6796813");
+        WebElement podbor = driver.findElementByName("clb6796813");
         podbor.click();
         //8. Убедиться что открыта страница "Киноафиша Москвы" и на форме выставлены заданные параметры.
 
-       // WebElement search = driver.findElement(By.id("header-search"));
-       // search.clear();
-       // search.sendKeys("наушники");
     }
 }
 
 
-/*
-1. Перейти на https://afisha.mail.ru/
-2. В блоке под поиском выбираем "Сходить в кино"
-3. Выбираем день "Завтра"
-4. Вводим метро "Курская" и выбираем из предложенных вариантов Курская (кольцевая)
-5. Выставляем жанр "Драма" и "Комедия"
-6. Ставим чекбокс "Только сеансы в 2D"
-7. Жмем "Подобрать"
-8. Убедиться что открыта страница "Киноафиша Москвы" и на форме выставлены заданные параметры.
+//Все действия необходимо логировать.
 
-Все действия необходимо логировать.
-
- */
